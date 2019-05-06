d8d3cf666886af27d93903c4bcce04c449718110
Артем Бурдак <artemboordak@gmail.com>
2019-05-06 18:41:38 +0300

    Added Google Places

--- EveryDayApp/WeatherControllers/WeatherVC.swift
+++ EveryDayApp/WeatherControllers/WeatherVC.swift
@@ -10,13 +10,14 @@ import UIKit
 import Alamofire
 import GooglePlaces
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, GMSAutocompleteViewControllerDelegate {
 
     @IBOutlet private weak var imageWeatherStatus: UIImageView!
     @IBOutlet private weak var weatherConditionLbl: UILabel!
     @IBOutlet private weak var tmpLbl: UILabel!
     @IBOutlet private weak var cityLabel: UILabel!
     @IBOutlet private weak var backgroundDayTimeImage: UIImageView!
+    @IBOutlet weak var changeCityTextField: UITextField!
 
     let locationManager = CLLocationManager()
 
@@ -60,7 +61,7 @@ class WeatherVC: UIViewController {
                 } else {
                     self.backgroundDayTimeImage.image = UIImage.init(named: "Day")
                 }
-//                self.cityLabel.text = self.weatherJson?.name
+                self.cityLabel.text = self.weatherJson?.name
                 self.weatherConditionLbl.text = self.weatherJson?.weather.first?.description
 
                 guard let tempResult = self.weatherJson?.main.temp else { return }
@@ -69,16 +70,23 @@ class WeatherVC: UIViewController {
         }
     }
 
-    func newCityName(data: Data) {
-        parce(json: data)
+    @IBAction func typeCityNameAction(_ sender: UITextField) {
+        changeCityTextField.resignFirstResponder()
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
     }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "changeCity" {
-            let destinationVC = segue.destination as! ChangeCityVC
-            destinationVC.delegate = self
-        }
-    }
+//
+//    func newCityName(data: Data) {
+//        parce(json: data)
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "changeCity" {
+//            let destinationVC = segue.destination as! ChangeCityVC
+//            destinationVC.delegate = self
+//        }
+//    }
 }
 
 extension WeatherVC: CLLocationManagerDelegate {
@@ -87,17 +95,42 @@ extension WeatherVC: CLLocationManagerDelegate {
                          didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count - 1]
         if location.horizontalAccuracy > 0 {
-            locationManager.stopUpdatingLocation()
 
             let latitude = String(location.coordinate.latitude)
             let longitude = String(location.coordinate.longitude)
 
-            let parameters: [String : String] = ["lat" : latitude,
+            let parameters: [String: String] = ["lat" : latitude,
                                                  "lon" : longitude,
                                                  "appid" : Constants.weatherKey]
 
             getWeatherData(url: Constants.weatherUrl, parameters : parameters)
+
+            locationManager.stopUpdatingLocation()
         }
+    }
+
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        
+        changeCityTextField.text = place.name
+        dismiss(animated: true, completion: nil)
+        cityLabel.text = changeCityTextField.text
+
+        if cityLabel.text?.isEmpty == false {
+
+            let parameters: [String: String] = ["lat" : String(place.coordinate.latitude),
+                                                "lon" : String(place.coordinate.longitude),
+                                                "appid" : Constants.weatherKey]
+
+            getWeatherData(url: Constants.weatherUrl, parameters : parameters)
+        }
+    }
+
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error: ", error.localizedDescription)
+    }
+
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
     }
 
     func locationManager(_ manager: CLLocationManager,
@@ -105,10 +138,6 @@ extension WeatherVC: CLLocationManagerDelegate {
         print(error)
         cityLabel.text = "Location error"
     }
-}
-
-extension WeatherVC: ChangeCityDelegate {
-
 }
 
 // MARK: - Private funcs -
